46e8a50460c1940c82788c3c7718da92066c5f35
Shubham Tiwari <snowflakelearnbyshubham@gmail.com>
2025-12-22 17:37:34 +0000

    done

--- models/Dim_customer.sql
+++ models/Dim_customer.sql
@@ -1,4 +1,4 @@
-{{config(materialized='table')}}
+{{ config(materialized='table') }}
 
 with customers as (
 
@@ -6,7 +6,6 @@ with customers as (
         id as customer_id,
         first_name,
         last_name
-
     from raw.jaffle_shop.customers
 
 ),
@@ -18,7 +17,6 @@ orders as (
         user_id as customer_id,
         order_date,
         status
-
     from raw.jaffle_shop.orders
 
 ),
@@ -27,32 +25,27 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
-
     from orders
-
-    group by 1
+    group by customer_id
 
 ),
-
 
 final as (
 
     select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
+        c.customer_id,
+        c.first_name,
+        c.last_name,
+        co.first_order_date,
+        co.most_recent_order_date,
+        coalesce(co.number_of_orders, 0) as number_of_orders
+    from customers c
+    left join customer_orders co
+        on c.customer_id = co.customer_id
 
 )
 
-select * from final
+select * from final;
