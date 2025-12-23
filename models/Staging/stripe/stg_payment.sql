ab1dd9a9fffe402f654f71f39ad0cf580fc1a6ea
Shubham Tiwari <snowflakelearnbyshubham@gmail.com>
2025-12-23 03:06:46 +0000

    f

---
+++ models/Staging/stripe/stg_payment.sql
@@ -0,0 +1,10 @@
+{{config(materialized='table',schema='staging')}}
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+amount,
+    created as created_at
+
+from stripe.payment
